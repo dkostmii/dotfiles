7bfe5f95961e48d845da81d4bdae89bfb8c3f979
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2024-02-14 18:45:02 +0200

    Update DWM config
    Add lock and screenshot shortcuts

--- dwm/config.h
+++ dwm/config.h
@@ -72,13 +72,17 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", "-f", st_font, "-e", st_shell, NULL };
+static const char *dmenucmd[]      = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]       = { "st", "-f", st_font, "-e", st_shell, NULL };
+static const char *lockcmd[]       = { "slock" };
+static const char *screenshotcmd[] = { "scrot", "-s", "/home/dkostmii/Pictures/Screenshots/Screenshot_%Y-%m-%d_%H-%M-%S.png", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_z,      spawn,          {.v = lockcmd } },
+	{ 0,                            XK_Print,  spawn,          {.v = screenshotcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
