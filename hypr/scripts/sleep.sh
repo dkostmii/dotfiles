c8b0c4a6d550789b306724a6bcaea38535281309
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2024-03-01 10:05:38 +0200

    Update Hyprland config
    Do not suspend the system. Lock simultaneously with display turn off.

--- hypr/scripts/sleep.sh
+++ hypr/scripts/sleep.sh
@@ -1,6 +1,4 @@
-swayidle -w timeout 300 'swaylock' \
-            timeout 600 'hyprctl dispatch dpms off' \
-            resume 'hyprctl dispatch dpms on' \
-            before-sleep 'swaylock' \
-            timeout 900 'systemctl suspend' \
+#!/bin/sh
+
+swayidle -w timeout 600 'hyprctl dispatch dpms off && swaylock' \
             resume 'hyprctl dispatch dpms on' &
