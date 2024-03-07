933a31e1c1bffb6bf58d1a862a964d71d5ca20f4
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2024-03-07 12:39:04 +0200

    Update Hyprland config
    Update environment variables. Disable sleep on idle in `sleep.sh` script.

--- hypr/scripts/sleep.sh
+++ hypr/scripts/sleep.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-swayidle -w timeout 600 'hyprctl dispatch dpms off && swaylock -f' resume 'hyprctl dispatch dpms on' \
-            timeout 630 'systemctl suspend && hyprctl dispatch dpms on' \
-            before-sleep 'swaylock -f' \
+swayidle -w timeout 600 'hyprctl dispatch dpms off && swaylock -f &' resume 'hyprctl dispatch dpms on' \
+            before-sleep 'hyprctl dispatch dpms on && swaylock -f &' \
             lock 'swaylock -f'
